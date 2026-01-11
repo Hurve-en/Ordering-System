be0eafbdaabe65950a497c9618b50fd061098c7f
Hurve-en <142562684+Hurve-en@users.noreply.github.com>
2026-01-11 20:33:06 +0800

    Update App.tsx

--- coffee-app/frontend/src/App.tsx
+++ coffee-app/frontend/src/App.tsx
@@ -1,47 +1,27 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { useAppSelector } from './hooks/useRedux';
-import { Navbar } from './components/Common/Navbar';
-import { Footer } from './components/Common/Footer';
-import { Login } from './pages/Login';
-import { Register } from './pages/Register';
-import { Home } from './pages/Home';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Common/Navbar';
+import Footer from './components/Common/Footer';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
-interface ProtectedRouteProps {
-  children: JSX.Element;
-}
-
-const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
-  const { isAuthenticated } = useAppSelector((state: any) => state.auth);
-  return isAuthenticated ? children : <Navigate to="/login" />;
-};
-
-function App() {
+const App: React.FC = () => {
   return (
     <Router>
       <div className="flex flex-col min-h-screen">
         <Navbar />
         <main className="flex-grow">
           <Routes>
-            {/* Public Routes */}
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
-
-            {/* Protected Routes - Coming in Section 3 */}
-            {/* <Route path="/menu" element={<Menu />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/checkout" element={<Checkout />} />
-            <Route path="/orders" element={<Orders />} />
-            <Route path="/profile" element={<Profile />} /> */}
-
-            {/* 404 */}
-            <Route path="*" element={<Navigate to="/" />} />
           </Routes>
         </main>
         <Footer />
       </div>
     </Router>
   );
-}
+};
 
 export default App;
